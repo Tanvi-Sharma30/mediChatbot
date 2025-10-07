b7a6b93954552501eef005fa459156bcc4515ef1
Tanvi-Sharma30 <tanvisha309@gmail.com>
2025-10-07 17:31:52 +0530

    Add project files and update template.sh

---
+++ template.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Creating directories
+mkdir -p src
+mkdir -p research 
+
+# Creating files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch requirements.txt
+
+echo "✅ Directories and files created successfully!"
